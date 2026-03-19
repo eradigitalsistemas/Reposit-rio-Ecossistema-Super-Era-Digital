0a79e16d9749aa0164f7e3309c09f80823762422
Skip <skip@usecurling.com>
2026-03-19 13:52:50 +0000

    feat: Add scrollbar to 'demandas' column

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -1,20 +1,7 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
 Deno.serve(async (req: Request) => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-      },
-    })
-  }
-
-  return new Response(JSON.stringify({ status: 'ok', message: 'Function running' }), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
+  return new Response(JSON.stringify({ message: 'Function executed successfully' }), {
+    headers: { 'Content-Type': 'application/json' },
   })
 })
