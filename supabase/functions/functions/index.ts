49a6af73977c5d1c1184d09922d5e9e3f7b001e3
Skip <skip@usecurling.com>
2026-03-19 13:43:32 +0000

    Feat: Add vertical scroll to Demands list

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -1,7 +1,20 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
 Deno.serve(async (req: Request) => {
-  return new Response(JSON.stringify({ status: 'ok', message: 'Functions entrypoint' }), {
-    headers: { 'Content-Type': 'application/json' },
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+      },
+    })
+  }
+
+  return new Response(JSON.stringify({ status: 'ok', message: 'Function running' }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
   })
 })
